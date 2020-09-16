b28741b2bb51db21f400d0263c5d62f2f3fe06f7
MattRul <mattias.ruljeff@gmail.com>
2020-09-16 15:36:30 +0200

    starting assignment, adding application.php

--- view/LoginView.php
+++ view/LoginView.php
@@ -71,7 +71,7 @@ class LoginView {
 	//CREATE GET-FUNCTIONS TO FETCH REQUEST VARIABLES
 	private function getRequestUserName() {
 		//RETURN REQUEST VARIABLE: USERNAME
-		return "username"
+		return "username";
 	}
 	
 }
