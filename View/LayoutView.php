d244e020d1b7786967cd0aeed9ea3e1e2425a22a
MattRul <mr223mi@student.lnu.se>
2020-09-18 10:06:56 +0200

    Hello

--- View/LayoutView.php
+++ View/LayoutView.php
@@ -3,38 +3,38 @@
 
 class LayoutView {
   
-  public function render($isLoggedIn, LoginView $v, DateTimeView $dtv, RegisterView $regView) {
-    if (session_status() == 1) {
-      echo "session started";
-      session_start();
-    }
+  // public function render($isLoggedIn, LoginView $v, DateTimeView $dtv, RegisterView $regView) {
+  //   if (session_status() == 1) {
+  //     echo "session started";
+  //     session_start();
+  //   }
 
-    echo '<!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8">
-          <title>Login Example</title>
-        </head>
-        <body>
-          <h1>Assignment 2</h1>
-          ' . $this->renderIsLoggedIn($isLoggedIn) . '
+  //   echo '<!DOCTYPE html>
+  //     <html>
+  //       <head>
+  //         <meta charset="utf-8">
+  //         <title>Login Example</title>
+  //       </head>
+  //       <body>
+  //         <h1>Assignment 2</h1>
+  //         ' . $this->renderIsLoggedIn($isLoggedIn) . '
           
-          <div class="container">
-              ' . $v->response() . '
+  //         <div class="container">
+  //             ' . $v->response() . '
               
-              ' . $dtv->show() . '
-          </div>
-         </body>
-      </html>
-    ';
-  }
+  //             ' . $dtv->show() . '
+  //         </div>
+  //        </body>
+  //     </html>
+  //   ';
+  // }
   
-  private function renderIsLoggedIn($isLoggedIn) {
-    if ($isLoggedIn) {
-      return '<h2>Logged in</h2>';
-    }
-    else {
-      return '<h2>Not logged in</h2>';
-    }
-  }
+  // private function renderIsLoggedIn($isLoggedIn) {
+  //   if ($isLoggedIn) {
+  //     return '<h2>Logged in</h2>';
+  //   }
+  //   else {
+  //     return '<h2>Not logged in</h2>';
+  //   }
+  // }
 }
