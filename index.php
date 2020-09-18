a0eb22f781c6ff42aa1aafb88df515638a16b639
MattRul <mr223mi@student.lnu.se>
2020-09-18 10:03:31 +0200

    Hello

--- index.php
+++ index.php
@@ -9,8 +9,8 @@ require_once("Application.php");
 
 require_once('view/LoginView.php');
 require_once('view/DateTimeView.php');
-require_once('view/LayoutView.php');
-// require_once("view/RegisterView.php");
+// require_once('view/LayoutView.php');
+require_once("view/RegisterView.php");
 // require_once("controller/logInOrOut.php");
 // require_once("model/UserStorage.php");
 // require_once("model/UserName.php");
