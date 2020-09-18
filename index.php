012e1d8e99e93f2999b452b800cd2d5c3fad5a5e
MattRul <mr223mi@student.lnu.se>
2020-09-18 09:48:01 +0200

    fixing heroku missing files

--- index.php
+++ index.php
@@ -5,7 +5,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-
+require_once('view/LoginView.php');
+require_once('view/DateTimeView.php');
+require_once('view/LayoutView.php');
+require_once("view/RegisterView.php");
+require_once("controller/logInOrOut.php");
+require_once("model/UserStorage.php");
+require_once("model/UserName.php");
+require_once("model/DatabaseConnection.php");
 require_once("Application.php");
 
 $app = new Application();
