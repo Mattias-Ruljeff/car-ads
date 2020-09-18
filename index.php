9c89ad6a7bd22ac0fcf9651485323957913007e8
MattRul <mr223mi@student.lnu.se>
2020-09-18 10:15:19 +0200

    Fixing heroku error...

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ require_once("Application.php");
 
 require_once('view/DateTimeView.php');
 require_once('view/LoginView.php');
+require_once("view/hej.php");
 require_once("view/LayoutView.php");
 // require_once("view/RegisterView.php");
 // require_once("controller/logInOrOut.php");
