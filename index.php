c0f7ff9cc16031cf38e459f8bb15eddf964e3642
MattRul <mr223mi@student.lnu.se>
2020-09-18 09:59:22 +0200

    test

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once( __DIR__  .'view/LoginView.php');
-require_once( __DIR__ . 'view/DateTimeView.php');
-require_once( __DIR__  . 'view/LayoutView.php');
-require_once( __DIR__  . "view/RegisterView.php");
+require_once( __DIR__  .'/view/LoginView.php');
+require_once( __DIR__ . '/view/DateTimeView.php');
+require_once( __DIR__  . '/view/LayoutView.php');
+require_once( __DIR__  . "/view/RegisterView.php");
 require_once("controller/logInOrOut.php");
 require_once("model/UserStorage.php");
 require_once("model/UserName.php");
