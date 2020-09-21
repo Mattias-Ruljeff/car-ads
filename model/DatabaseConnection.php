3e6d02ceed23a145a1c51b0934dfd2bc2b37dd0c
MattRul <mr223mi@student.lnu.se>
2020-09-21 14:10:44 +0200

    fixing .env on heroku

--- model/DatabaseConnection.php
+++ model/DatabaseConnection.php
@@ -11,17 +11,17 @@ class DatabaseConnection {
 
     // Heroku
     // private $dbparts = $_SERVER["SERVER_NAME"] == "localhost" ? "localhost:8889" : parse_url($url);
-    private $url = getenv('JAWSDB_URL');
-    private $dbparts = parse_url($url);
     
     private $hostname = $dbparts['host'];
     private $username = $dbparts['user'];
     private $password = $dbparts['pass'];
     private $database = ltrim($dbparts['path'],'/');
-
+    
     public function __construct(){     
+        $url = getenv('JAWSDB_URL');
+        $dbparts = parse_url($url);
         // Create connection
-        $this->dbConnection = new mysqli($this->dbparts, $this->username, $this->password, $this->hostname);
+        $this->dbConnection = new mysqli($dbparts, $this->username, $this->password, $this->hostname);
     }
 
     private function createDatabase () {
