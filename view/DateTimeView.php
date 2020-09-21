7103ff104328bca6a72424d2afc04e1be3ed1d63
MattRul <mr223mi@student.lnu.se>
2020-09-21 16:00:41 +0200

    fixing log in error messages

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -6,7 +6,7 @@ class DateTimeView {
 	public function show() {
 
 		date_default_timezone_set("Europe/Stockholm");
-		$timeString = date('l jS \of F Y H:i:s');
+		$timeString = date('l jS \of F Y') . ", The time is " . date("H:i:s");
 
 		return '<p>' . $timeString . '</p>';
 	}
