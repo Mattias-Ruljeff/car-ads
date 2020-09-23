d6a573b99b661021026f031732d49cc6f335b594
MattRul <mr223mi@student.lnu.se>
2020-09-23 13:54:44 +0200

    Added logout button and function

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 require_once("Application.php");
 
@@ -13,4 +13,4 @@ $app->run();
 if(isset($_SERVER['HTTP_CACHE_CONTROL'])){
     echo " refresh ";
 };
-var_dump($_SESSION);
+// var_dump($_SESSION);
