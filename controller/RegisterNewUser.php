0a9032d9a6ba2290f50219f37825c0ed4acb34c4
MattRul <mr223mi@student.lnu.se>
2020-09-24 10:46:54 +0200

    Fixing error messages in registerform

--- controller/RegisterNewUser.php
+++ controller/RegisterNewUser.php
@@ -21,7 +21,7 @@ class RegisterNewUser {
                 $repeatedPasswrd = $this->view->getRepeatedPassword();
 
                 if(!$username and !$passwrd){
-                    return "Username has too few characters, at least 3 characters.'\n'Password has too few characters, at least 6 characters.";
+                    return "Username has too few characters, at least 3 characters.<br>Password has too few characters, at least 6 characters.";
                 }
 
                 // Check username-----------------------------------------
