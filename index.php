1891252d3515634ef83803cf86b179113aaaf592
MattRul <mr223mi@student.lnu.se>
2020-09-24 09:56:09 +0200

    Adding new user to database working

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
 
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 require_once("Application.php");
 
 $app = new Application();
 $app->run();
 
-if(isset($_SERVER['HTTP_CACHE_CONTROL'])){
-    // echo " refresh ";
-};
 // var_dump($_SESSION);
