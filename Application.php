ae64e00ffbb654eb53ca772981e5f7d23de5cd2c
MattRul <mattias.ruljeff@gmail.com>
2020-10-14 21:43:10 +0200

    push to heroku

--- Application.php
+++ Application.php
@@ -15,7 +15,6 @@ require_once("model/DatabaseConnection.php");
 require_once("model/SessionModel.php"); 
 require_once("model/AdsModel.php"); 
 
-//MAKE SURE ERRORS ARE SHOWN... MIGHT WANT TO TURN THIS OFF ON A PUBLIC SERVER
 // error_reporting(E_ALL);
 // ini_set('display_errors', 'On');
 
